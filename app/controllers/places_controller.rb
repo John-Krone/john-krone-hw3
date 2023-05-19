59b625ad6a67f2079f0f8337787733f911db065f
John Krone <john.krone@kellogg.northwestern.edu>
2023-05-19 16:43:17 +0000

    finished controllers/show.new pages

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -5,10 +5,32 @@ def index
 @places = Place.all
 end
 
+def show
+  
+    @place = Place.find_by({"id" => params["id"]})
+    @posts = Post.where({"place_id" => @place["id"]})
+  
+  end
 
+   def new
+    @place = Place.new
+    
+  #   # render view with new Company form
+   end
+  
+  def create
+  #   # start with a new Company
+  @place = Place.new
 
+    puts params["place"]
+  #   # assign user-entered form data to Company's columns
+    @place["name"] = params["place"] ["name"]
 
-
+  
+  @place.save
+  #   # redirect user
+  redirect_to "/place"
+   end
 
 
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,38 @@
 class PostsController < ApplicationController
 
 
-def index
-  @posts = Post.all
+  def show
+    @post = Post.find_by({"id" => params["id"]})
+    @place = Place.where({"id" => @post["place_id"]})
+    
+  end
+
+  def new
+    @post = Post.new
+    @post["place_id"] = params["place_id"]
+    
+  end
+
+  def create
+    # start with a new Contact
+    @contact = Contact.new
+
+    # assign user-entered form data to Contact's columns
+    @post["title"] = params["post"]["title"]
+    @post["posted_on"] = params["post"]["posten_on"]
+    
+    
+
+    # assign relationship
+    @post["place_id"] = params["post"]["place_id"]
+
+    # save Contact row
+    @post.save
+
+    # redirect user
+    redirect_to "/companies/#{@contact["company_id"]}"
+  end
+
 end
 
 
@@ -14,4 +44,4 @@ end
 
 
 
-end
+
